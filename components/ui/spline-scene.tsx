e3bbbc051758ea5cd7e68c2fe22c9cc241bc2b52
mridul2620 <mridulsriv26@gmail.com>
2026-03-11 00:03:48 +0000

    spline effect added

--- components/ui/spline-scene.tsx
+++ components/ui/spline-scene.tsx
@@ -1,23 +1,12 @@
-"use client"
+'use client'
 
-import { useEffect, useState } from "react"
+import dynamic from 'next/dynamic'
+
+const Spline = dynamic(
+  () => import('@splinetool/react-spline').then((m) => m.default),
+  { ssr: false }
+)
 
 export function SplineScene({ scene, className }: { scene: string; className?: string }) {
-  const [Spline, setSpline] = useState<any>(null)
-
-  useEffect(() => {
-    import("@splinetool/react-spline").then((mod) => {
-      setSpline(() => mod.default)
-    })
-  }, [])
-
-  if (!Spline) {
-    return (
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="w-8 h-8 border-2 border-primary border-t-transparent rounded-full animate-spin" />
-      </div>
-    )
-  }
-
   return <Spline scene={scene} className={className} />
 }
